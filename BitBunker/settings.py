51d5e83562363461735e8ef6a75f9bb35ec94b57
xxghoulpr0xx <javieralvarado1102@gmail.com>
2024-09-09 14:26:53 -0600

    Entorno virtual agregado

--- BitBunker/settings.py
+++ BitBunker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q52!s$73ko!!()^(+@i$@z*e&vzur#t7+*%vfjt71=$k&jf7co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    BASE_DIR, 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
