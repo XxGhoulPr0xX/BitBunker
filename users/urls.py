49a1a50c125c093d9b497f0ecf9da0428aead80b
xxmiluxx <mendoza21diego@gmail.com>
2024-09-19 17:03:41 -0600

    Guardando cambios locales antes del pull

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('profile/', profile, name="profile"),
     path('home/', home, name="Home"),
-    path('registrarse', registrarse, name="registrarse")
+    path('registrarse', registrarse, name="registrarse"),
+    path('minecraft', minecraft, name="minecraft")
 ]
