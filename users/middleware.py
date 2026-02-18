21c03c45a59129e346ef0cb69659ca6cf715f1ac
XxGhoulPr0xX <javieralvaradomarquez1102@gmail.com>
2026-02-17 18:23:11 -0600

    Cambios de inicio de sesión

--- users/middleware.py
+++ users/middleware.py
@@ -6,7 +6,11 @@ class LoginRequiredMiddleware:
 
     def __call__(self, request):
         public_paths = ['/login/', '/register/', '/index/'] 
-        if not request.session.get('matricula'):
+        usuario_tiene_sesion = request.session.get('matricula')
+        if usuario_tiene_sesion:
+            if request.path in public_paths or request.path == '/':
+                return redirect('home')
+        else:
             if request.path == '/':
                 return redirect('inicio')
             if request.path not in public_paths:
